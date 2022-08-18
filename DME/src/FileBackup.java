811e7e1b343ecb10b66945fbcaa57f6492cf3dc0
mw612 <46521570+mw612@users.noreply.github.com>
2022-08-18 13:24:49 +0200

    Adding Backup Class

--- DME/src/FileBackup.java
+++ DME/src/FileBackup.java
@@ -1,15 +1,33 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FileBackup {
-	private String path;
-	private String destination;
+
+	private Path src;
+	private Path dest;
+	
 	
 	FileBackup(String path, String destination){
-		this.destination = destination;
-		this.path = path;
+		src = Paths.get(path);
+		
+		final String timestamp = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(new Date());
+		destination = timestamp + "-" + destination;
+		dest = Paths.get(destination);
 	}
 	
+	
 	private void backupFile() {
-		
+		try {
+			Files.copy(src, dest, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
